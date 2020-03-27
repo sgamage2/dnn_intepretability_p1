aba7e4f27eb0bfa0381fdad892305734650174d6
wzmdaisy <60621350+wzmdaisy@users.noreply.github.com>
2020-03-27 18:24:49 -0400

    Update and rename Wudan.txt to Wudan.py

---
+++ Wudan_tests/Wudan.py
@@ -0,0 +1,9 @@
+#first neural network using keras in python
+  ##########################
+  #1. load Pima Indians onset of diabetes dataset
+  
+
+from numpy import loadtxt
+from keras.models import Sequential
+from keras.layers import Dense
+
