8232391de7418c4a1152d4234932d6040f681bf3
wzmdaisy <60621350+wzmdaisy@users.noreply.github.com>
2020-03-27 20:06:29 -0400

    Delete Wudan.py

--- Wudan_tests/Wudan.py
+++
@@ -1,9 +0,0 @@
-#first neural network using keras in python
-  ##########################
-  #1. load Pima Indians onset of diabetes dataset
-  
-
-from numpy import loadtxt
-from keras.models import Sequential
-from keras.layers import Dense
-
