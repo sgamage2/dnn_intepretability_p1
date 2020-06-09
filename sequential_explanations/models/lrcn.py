9bf4100d8781a712c23914a158fd62af1bfbe367
Sunanda <sgamage2@uwo.ca>
2020-06-08 21:43:59 -0400

    Working LRCN model, training, loading video frames and testing

--- sequential_explanations/models/lrcn.py
+++ sequential_explanations/models/lrcn.py
@@ -1,11 +1,12 @@
 import tensorflow as tf
 
 from tensorflow.keras.models import Sequential, Model
-from tensorflow.keras.layers import LSTM, Dense, Dropout, TimeDistributed, BatchNormalization, Bidirectional, Reshape
+from tensorflow.keras.layers import LSTM, Dense, Dropout, TimeDistributed, BatchNormalization, Bidirectional, Reshape, Input
 from tensorflow.keras.callbacks import EarlyStopping, TensorBoard
 import logging
 from models.keras_callbacks import WeightRestorer
-from tensorflow.keras.applications import resnet
+# from tensorflow.keras.applications import resnet
+from tensorflow.keras.applications import inception_v3
 
 
 # Wrapper class for the LRCN model (CNN + LSTM)
@@ -16,63 +17,139 @@ class LRCNClassifier:
     def initialize(self, exp_params):
         self.params = exp_params
         self.params['model'] = 'lrcn'    # As a marker
-        latent_dim = self.params['latent_dim']    # No. of features of LSTM input
-        layer_nodes = exp_params['lstm_layer_units']
-        num_classes = exp_params['output_nodes']
-        time_steps = exp_params['lstm_time_steps']
 
+        self.cnn = self.create_cnn(exp_params)
+        self.lstm = self.create_lstm(exp_params)
+        self.lrcn_model = self.create_lrcn(exp_params)
 
         # ---------------------------
-        # CNN part of the LRCN model
+        # Full LRCN model (CNN + LSTM)
 
+        # self.lrcn_model = Model(inputs=cnn_base.input, outputs=classifications)
+
+        # self.lrcn_model.compile(optimizer='adam', loss='binary_crossentropy')
+
+        logger = logging.getLogger(__name__)
+        self.lrcn_model.summary(print_fn=logger.info)
+
+        # preproc_func = resnet.preprocess_input
+        # decode_preds_func = resnet.decode_predictions
+
+    def create_cnn(self, params):
         # Keep the last Global Avg Pooling layer (but not the final Dense [softmax] layer with 1000 nodes for classes)
-        cnn_base = resnet.ResNet152(weights='imagenet', pooling='avg', include_top=False)
+        cnn_base = inception_v3.InceptionV3(weights='imagenet', pooling='avg', include_top=False)
 
         # self.cnn_base_with_top = resnet.ResNet152(weights='imagenet', include_top=True)   # For comparison
 
         for layer in cnn_base.layers:
             layer.trainable = False
 
-        a = cnn_base.output
-        cnn_out = Dense(latent_dim, activation=None)(a)
+        # a = cnn_base.output
+        # cnn_out = Dense(latent_dim, activation=None)(a)
+        cnn_out = cnn_base.output
 
-        # self.cnn_full_model = Model(inputs=self.cnn_base.input, outputs=cnn_out)    # Keep aside the CNN full model (unused)
+        cnn_model = Model(inputs=cnn_base.input, outputs=cnn_out)
+
+        return cnn_model
+
+    def create_lstm(self, params):
+        latent_dim = params['latent_dim']  # No. of features of LSTM input
+        layer_nodes = params['lstm_layer_units']
+        num_classes = params['output_nodes']
+        time_steps = params['lstm_time_steps']
+        dropouts = params['lstm_layer_dropout_rates']
+
+        lstm = Sequential()
+
+        ret_seq = False if len(layer_nodes) == 1 else True  # To handle only 1 hidden layer
+
+        # First hidden layer (need to specify input layer nodes here)
+        lstm.add(LSTM(units=layer_nodes[0], return_sequences=ret_seq, input_shape=(time_steps, latent_dim)))
+        lstm.add(Dropout(dropouts[0]))
+
+        # Other hidden layers
+        for i in range(1, len(layer_nodes)):
+            ret_seq = False if i == len(layer_nodes) - 1 else True    # Last hidden layer should not return sequences
+            lstm.add(Bidirectional(LSTM(units=layer_nodes[i], return_sequences=ret_seq)))
+            lstm.add(Dropout(dropouts[i]))
+
+        lstm.add(Dense(num_classes, activation='softmax'))
+
+        lstm.compile(optimizer='adam', loss='categorical_crossentropy',
+                     metrics=['accuracy','top_k_categorical_accuracy'])
+
+        return lstm
+
+    def create_lrcn(self, params):
+        time_steps = params['lstm_time_steps']
+        width, height, channels = params['image_shape']
+
+        cnn_input = Input(shape=(time_steps, width, height, channels))  # input shape = (num_samples, time_steps, w, h, channels)
+
+        cnn_model = TimeDistributed(Model(inputs=self.cnn.input, outputs=self.cnn.output))
+        lstm_model = Model(inputs=self.lstm.input, outputs=self.lstm.output)
+
+        cnn_output = cnn_model(cnn_input)
+        lstm_output = lstm_model(cnn_output)
+
+        lrcn_model = Model(cnn_input, lstm_output)
+
+        return lrcn_model
+
+    def fit_lstm(self, X_train, y_train, X_valid=None, y_valid=None):
+        epochs = self.params['lstm_epochs']
+        batch_size = self.params['lstm_batch_size']
+        early_stop_patience = self.params['lstm_early_stop_patience']
 
 
-        # ---------------------------
-        # LSTM part of the LRCN model
+        # --------------------------------
+        # Create callbacks
 
-        lstm_layer_in = Reshape((time_steps, latent_dim))(cnn_out)
+        # tensorboard_cb = TensorBoard(log_dir=self.params['tensorboard_log_dir'], batch_size=batch_size)
+        # callbacks = [tensorboard_cb]
+        callbacks = []
 
-        for nodes in layer_nodes:
-            lstm_layer_in = Bidirectional(LSTM(nodes, return_sequences=True))(lstm_layer_in)
+        # Early stopping is enabled
+        if X_valid is not None and early_stop_patience > 0 and early_stop_patience < epochs:
+            early_stopping_cb = EarlyStopping(monitor='val_loss', patience=early_stop_patience,
+                                              verbose=2, mode='auto', restore_best_weights=False)
+            callbacks.append(early_stopping_cb)
 
-        classifications = TimeDistributed(Dense(num_classes, activation='softmax'))(lstm_layer_in)
+        if X_valid is not None:
+            weight_restorer_cb = WeightRestorer(epochs)
+            callbacks.append(weight_restorer_cb)
 
-        # self.lstm_model = Model(inputs=cnn_out, outputs=classifications)      # Keep aside the LSTM full model (unused)
+        # --------------------------------
+        # Fit
 
+        if X_valid is not None and y_valid is not None:
+            validation_data = (X_valid, y_valid)
+        else:
+            validation_data = None
 
-        # ---------------------------
-        # Full LRCN model (CNN + LSTM)
+        history = self.lstm.fit(X_train, y_train, epochs=epochs, batch_size=batch_size,
+                               validation_data=validation_data, callbacks=callbacks, verbose=2)
 
-        self.lrcn_model = Model(inputs=cnn_base.input, outputs=classifications)
+        return history
 
-        self.lrcn_model.compile(optimizer='adam', loss='binary_crossentropy')
-
-        logger = logging.getLogger(__name__)
-        # self.lrcn_model.summary(print_fn=logger.info)
-
-        preproc_func = resnet.preprocess_input
-        decode_preds_func = resnet.decode_predictions
+    def predict_lrcn(self, X_video_frames):
+        return self.lrcn_model.predict(X_video_frames)
+        # return self.cnn.predict(X_video_frames)
 
     def get_tf_model(self):
-        return self.lrcn_model
+        return (self.cnn, self.lstm, self.lrcn_model)
 
-    def set_tf_model(self, model):
-        self.lrcn_model = model
+    def set_tf_model(self, models):
+        if models is None:
+            self.cnn, self.lstm, self.lrcn_model = None, None, None
+        else:
+            self.cnn, self.lstm, self.lrcn_model = models
 
     def save(self, filename):
-        self.lrcn_model.save(filename, overwrite=True, save_format='h5')
+        self.lstm.save(filename, overwrite=True, save_format='h5')
 
     def load_model(self, filename):
-        self.lrcn_model = tf.keras.models.load_model(filename)
+        self.lstm = tf.keras.models.load_model(filename)
+        self.cnn = self.create_cnn(self.params)
+        self.lrcn_model = self.create_lrcn(self.params)
+
--- sequential_explanations/models/lrcn_ucf_video_classification.py
+++ sequential_explanations/models/lrcn_ucf_video_classification.py
@@ -1,39 +1,93 @@
 import logging, time, os
-
 import models.lrcn
 import utility
-
+from video_utility.data import DataSet
+import numpy as np
 
 exp_params = {}
+exp_params['data_base_path'] = '/home/jiazhi/videoucftest/data'
+exp_params['sequences_path'] = '/home/jiazhi/videoucftest/data/sequences111'
 exp_params['results_dir'] = 'output'
 exp_params['exp_id'] = 'lrcn_1'
-exp_params['latent_dim'] = 512  # No. of features of LSTM input
+exp_params['image_shape'] = (224, 224, 3)
+exp_params['latent_dim'] = 2048  # No. of features of LSTM input
 exp_params['lstm_layer_units'] = [256, 256]
-exp_params['output_nodes'] = 55  # No. of classes
-exp_params['lstm_time_steps'] = 5   # No. of frames to classify at one time (LSTM unrolling)
+exp_params['lstm_layer_dropout_rates'] = [0.2, 0.2]
+exp_params['output_nodes'] = 70  # No. of classes
+exp_params['lstm_time_steps'] = 40   # No. of frames to classify at one time (LSTM unrolling)
+exp_params['lstm_batch_size'] = 64
+exp_params['lstm_epochs'] = 10
+exp_params['lstm_early_stop_patience'] = -1  # Disabled
 
-def create_lrcn(params):
+
+def create_and_train_lrcn(X_train, y_train, X_test, y_test, params):
     model = models.lrcn.LRCNClassifier()
     model.initialize(params)
-    return model
+
+    logging.info('Training model')
+    t0 = time.time()
+
+    # history = None
+    history = model.fit_lstm(X_train, y_train, X_test, y_test)
+
+    time_to_train = time.time() - t0
+    logging.info('Training complete. time_to_train = {:.2f} sec, {:.2f} min'.format(time_to_train, time_to_train / 60))
+
+    return model, history
+
+
+def evaluate_model(model, X, y_true, dataset_name):
+    logging.info('{}: Predicting and evaluating model'.format(dataset_name))
+
+    y_pred = model.predict_lrcn(X)
+
+    # Integer labels
+    y_pred = y_pred.argmax(axis=1)
+    y_true = y_true.argmax(axis=1)
+
+    utility.print_evaluation_report(y_true, y_pred, dataset_name)
 
 
 def main():
-    os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+    # os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
     utility.initialize(exp_params)
 
-    model = create_lrcn(exp_params)
+    # ------------------------
+    # Load sequences of features from CNN last layer
 
-    # 2 Options for here:
-    # 1) Transfer weights from pytorch model to keras LRCN model (models.lrcn)
-    # 2) Train LRCN from scratch
+    dataset = DataSet(seq_length=exp_params['lstm_time_steps'],
+                   class_limit=exp_params['output_nodes'],
+                   image_shape=exp_params['image_shape'],
+                   base_path=exp_params['data_base_path'],
+                   sequences_path=exp_params['sequences_path'])
+
+    X_train, y_train = dataset.get_all_sequences_in_memory('train', 'features')
+    X_test, y_test = dataset.get_all_sequences_in_memory('test', 'features')
+
+    logging.info('X_train.shape = {}, y_train.shape = {}'.format(X_train.shape, y_train.shape))
+    logging.info('X_test.shape = {}, y_test.shape = {}'.format(X_test.shape, y_test.shape))
+
+    # ------------------------
+    # Train model
+    model, history = create_and_train_lrcn(X_train, y_train, X_test, y_test, exp_params)
+
+    # dummy_videos = np.random.randn(5, 40, 229, 229, 3)
+    # preds = model.predict_lrcn(dummy_videos)
+    # print('preds.shape = {}'.format(preds.shape))
 
     utility.save_model(model, exp_params['results_dir'])
 
-    model = utility.load_model(exp_params['results_dir'])  # For testing
+    # model = utility.load_model(exp_params['results_dir'])  # For testing
 
-    print(model)
+    # ------------------------
+    # Evaluate model on test set videos (img seqs)
+
+    X_video_test, y_video_test = dataset.get_frames_for_sample_set('test', num_samples=8)
+    logging.info('X_test.shape = {}, y_test.shape = {}'.format(X_video_test.shape, y_video_test.shape))
+
+    evaluate_model(model, X_video_test, y_video_test, "Test set")
+
 
 if __name__ == "__main__":
     main()
